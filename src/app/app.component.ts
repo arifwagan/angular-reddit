bdabc1966b90bbffd626d9a2973454d916dcf4fe
arif <arifhussainwagan@hotmail.com>
2019-02-28 15:55:53 +0500

    message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
+import { Article } from './article/article.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
+
 export class AppComponent {
-  title = 'angular-reddit';
-  addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
-     console.log(`Adding article title: ${title.value} and link: ${link.value}`);
-     return false;
+
+  articles: Article[]; // <-- component property
+
+ constructor() {
+      this.articles = [
+      new Article('Angular 2', 'http://angular.io', 3),
+      new Article('Fullstack', 'http://fullstack.io', 2),
+      new Article('Angular Homepage', 'http://angular.io', 1),
+      ];
+
   }
 }
